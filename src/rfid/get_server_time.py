c6e0f9518feb6c8270e4b2f1b44d2cdbb267e276
Matthew Berrington <berrington95@gmail.com>
2025-03-23 08:44:15 +1100

    create src layout

--- src/rfid/get_server_time.py
+++ src/rfid/get_server_time.py
@@ -1,11 +1,11 @@
 import paramiko
 
-host = "169.254.1.1"
-host = "speedwayr-12-36-0F"
+# host = "169.254.1.1"
+# host = "speedwayr-12-36-0F"
 host = "speedwayr-12-36-0F.local"
 username = "root"
 password = "impinj"
-command = "show system SysTime"
+command = "show system summary"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
