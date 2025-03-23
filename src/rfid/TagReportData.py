3d453202a8e9e1f2439aa3b8bc8a61aa84150483
Matthew Berrington <berrington95@gmail.com>
2025-03-23 15:42:57 +1100

    Added TagReportData class with csv export

---
+++ src/rfid/TagReportData.py
@@ -0,0 +1,52 @@
+import datetime
+import csv
+import os
+class TagReportData:
+    def __init__(self, config_dict, tag_report):
+        self.EPC = int(tag_report['EPC'], 16)
+
+        for content, enabled in config_dict['tag_content_selector'].items():
+            attr = enabler_to_key[content]
+            if enabled:
+                # if 'Timestamp' in content:
+                #     timestamp = datetime.datetime.fromtimestamp(tag_report[attr]/1000000) #convert from us to s
+                #     setattr(self, attr, timestamp)
+                # else:
+                setattr(self, attr, tag_report[attr])
+            else:
+                setattr(self, attr, None)
+
+    
+    def export_to_csv(self, filepath):
+        columns = ['EPC',*enabler_to_key.values()]
+        if not os.path.isfile(filepath):
+            with open(filepath, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(columns)
+
+        with open(filepath, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([self.EPC,
+                     self.ROSpecID,
+                     self.SpecIndex,
+                     self.InventoryParameterSpecID,
+                     self.AntennaID,
+                     self.PeakRSSI,
+                     self.ChannelIndex,
+                     self.FirstSeenTimestampUTC,
+                     self.LastSeenTimestampUTC,
+                     self.TagSeenCount,
+                     self.AccessSpecID])
+
+enabler_to_key = {
+    'EnableROSpecID': 'ROSpecID',
+    'EnableSpecIndex': 'SpecIndex',
+    'EnableInventoryParameterSpecID': 'InventoryParameterSpecID',
+    'EnableAntennaID': 'AntennaID',
+    'EnablePeakRSSI': 'PeakRSSI',
+    'EnableChannelIndex': 'ChannelIndex',
+    'EnableFirstSeenTimestamp': 'FirstSeenTimestampUTC',
+    'EnableLastSeenTimestamp': 'LastSeenTimestampUTC',
+    'EnableTagSeenCount': 'TagSeenCount',
+    'EnableAccessSpecID': 'AccessSpecID',
+}  
--- src/rfid/get_server_time.py
+++ src/rfid/get_server_time.py
@@ -1,28 +1,37 @@
+import datetime
 import paramiko
-from datetime import datetime
 
 def send_command(host, username, password, command):
-	client = paramiko.client.SSHClient()
-	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	client.connect(host, username=username, password=password)
-	_stdin, _stdout,_stderr = client.exec_command(command)
-	client.close()
-	return _stdout.read().decode()
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password)
+    _stdin, _stdout,_stderr = client.exec_command(command)
+    if not _stderr.read() == b'':
+        print('Error!')
+    else:
+        ret = _stdout.read()
+    client.close()
+    return ret
 
 def get_time_UTC(host, username, password):
-	command = "show system summary"
-	ret = send_command(host, username, password, command)
-	print(ret)
+    command = "show system summary"
+    ret = send_command(host, username, password, command)
+    response_list = ret.decode().split('\n')
+    rfid_time_UTC = datetime.datetime.strptime(response_list[5][9:-1],  '%a %b %d %H:%M:%S %Z %Y')
+    return rfid_time_UTC
 
 def set_time_UTC(host, username, password):
-	t = datetime.utcnow()
-	command = "config system time " + t.strftime("%Y.%m.%d-%H:%M:%S")
-	ret = send_command(host, username, password, command)
-	print(ret)
+    t = datetime.datetime.now(datetime.timezone.utc)
+    command = "config system time " + t.strftime("%Y.%m.%d-%H:%M:%S")
+    ret = send_command(host, username, password, command)
+    return ret
 
 if __name__ == '__main__':
-	# host = "169.254.1.1"
-	# host = "speedwayr-12-36-0F"
-	host = "speedwayr-12-36-0F.local"
-	username = "root"
-	password = "impinj"
+    host = "speedwayr-12-36-0F.local"
+    username = "root"
+    password = "impinj"
+    
+    # set_time_UTC(host, username, password)
+
+    print(get_time_UTC(host, username, password))
+    print(datetime.datetime.now(datetime.timezone.utc))
